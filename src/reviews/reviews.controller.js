7cff2114fd79d3e332c9a1c16d5fbf9a09af7c91
RachelSparto <rmbachman2016@hotmail.com>
2022-04-01 13:38:55 -0400

    reviews route working

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,4 +1,33 @@
 const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-module.exports = {};
+async function reviewExists(req, res, next) {
+  const { reviewId } = req.params;
+
+  const foundReview = await service.read(reviewId);
+
+  if (foundReview) {
+    res.locals.review = foundReview;
+    return next();
+  }
+  next({ status: 404, message: "Review cannot be found." });
+}
+
+async function destroy(req, res) {
+  const { reviewId } = req.params;
+  await service.delete(reviewId);
+  res.sendStatus(204);
+}
+
+async function update(req, res) {
+  const { reviewId } = req.params;
+  const updatedReview = { ...res.locals.review, ...req.body.data };
+  await service.update(updatedReview);
+  const returnData = await service.getReviewsWithCritic(reviewId);
+  res.json({ data: returnData });
+}
+
+module.exports = {
+  delete: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
+  update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
+};
